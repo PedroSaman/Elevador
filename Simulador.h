187125f7ed9be7126a7d14c25f5dde75da354f78
Pedro Saman <pedrosaman96@gmail.com>
2016-09-16 19:22:05 -0300

    Update Simulador.h
    Adaptação para o getopt

--- Simulador.h
+++ Simulador.h
@@ -2,6 +2,6 @@
 #define Simulador
 
 int Chamou_Elevador_FIFO(TipoFila *Fila, int Linha_Do_Tempo, int Andar_inicial);
-void Carrega_Fila_com_os_Dados(TipoFila *Fila);
+void Carrega_Fila_com_os_Dados(TipoFila *Fila, char Qual_Arquivo[]);
 
 #endif
