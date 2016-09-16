bc7328a215ca11045794f104692ef67b8e780a28
Pedro Saman <pedrosaman96@gmail.com>
2016-09-16 14:03:25 -0300

    Update main.c
    Assim a main fica focada no getopt

--- main.c
+++ main.c
@@ -5,26 +5,11 @@
 
 int main (){
 
-	FILE *fp=NULL;
 	int andar_maximo,numero_de_eventos,carga_maxima, Linha_Do_Tempo = 0, Andar_Inicial = 1;
 	TipoFila Fila;
-	TipoItem Info;
 
 	Faz_Fila_Vazia(&Fila);
-	fp = fopen("arquivo_com_as_entradas.txt","r");
-
-	if(fp == NULL){
-		printf("Erro ao tentar abrir o arquivo\n");
-	}
-
-	fscanf(fp,"%d %d %d", &andar_maximo,&numero_de_eventos,&carga_maxima);
-
-	while(numero_de_eventos>0){
-
-		fscanf(fp,"%d %d %d",&Info.andar_de_origem,&Info.andar_de_destino,&Info.tempo_de_chamada);
-		Enfileira(Info, &Fila);
-		numero_de_eventos--;
-	}
+	Carrega_Fila_com_os_Dados(&Fila);
 
 	Chamou_Elevador_FIFO(&Fila,Linha_Do_Tempo, Andar_Inicial);
 
