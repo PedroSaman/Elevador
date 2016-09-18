8e82c80596ed8b0c78587cda57882da65cb2889a
Pedro Saman <pedrosaman96@gmail.com>
2016-09-18 14:00:14 -0300

    Update main.c
    Adicionou-se o simulador inteligente mudando assim o getopt

--- main.c
+++ main.c
@@ -10,31 +10,39 @@
 
 int main(int argc, char *argv[]){
 
-	int andar_maximo,numero_de_eventos,carga_maxima, Linha_Do_Tempo = 0, Andar_Inicial = 1, x;
+	int andar_maximo,carga_maxima, numero_de_eventos, Tempo_Inicial=0, Andar_inicial=1, x;
 	TipoFila Fila;
 	struct rusage uso_inicio, uso_final;    
 	struct timeval inicio, fim;             /*Variáveis para calcular tempo de execução e uso de memória*/
 
 	getrusage(RUSAGE_SELF, &uso_inicio);
-	gettimeofday(&inicio, NULL);
 
 	Faz_Fila_Vazia(&Fila);
 	
 	while((x = getopt(argc, argv, "a:t:h"))!=-1){
 		switch(x){
 			case 'a':
-					Carrega_Fila_com_os_Dados(&Fila,optarg);
+					Carrega_Fila_com_os_Dados(&Fila,optarg,&andar_maximo,&numero_de_eventos,&carga_maxima);
 				break;
 			case 't':
 					if (strcmp (optarg,"FIFO") == 0){
 						printf("------------------ Você escolheu o Simulador FIFO -------------------\n\n");
-						Chamou_Elevador_FIFO(&Fila,Linha_Do_Tempo, Andar_Inicial);
-					}/*else if(optarg == 'OUTRO') FUNCAO COM A LÓGICA OUTRO*/
+						gettimeofday(&inicio, NULL);
+						Chamou_Elevador_FIFO(&Fila,Tempo_Inicial, Andar_inicial);
+						gettimeofday(&fim, NULL);
+
+					}else if(strcmp (optarg,"Inteligente") == 0){
+						printf("---------------- Você escolheu o Simulador Inteligente ----------------\n\n");
+						gettimeofday(&inicio, NULL);
+						Chamou_Elevador_Inteligente(&Fila, carga_maxima);
+						gettimeofday(&fim, NULL);
+
+					}
 				break;
 			case 'h':
 					printf("***************************************************\n***********************Ajuda***********************\n***************************************************\n");
 					printf("\t -t Para escolher qual simulador usar. EX: -f FIFO\t");
-					printf("Disponiveis: FIFO e OUTRO\n");
+					printf("Disponiveis: FIFO e Inteligente\n");
 					printf("\t -a Para escolher qual arquivo.txt com os dados de entrada. EX: -a nome_do_arquivo ");
 					printf("OBS: O arquivo tem que estar no mesmo diretório\n");
 					printf("\t -h Para mostrar ajuda\n");
@@ -45,7 +53,6 @@ int main(int argc, char *argv[]){
 		}	
 	}
 	getrusage(RUSAGE_SELF, &uso_final);
-	gettimeofday(&fim, NULL);
 	printf("\n\n\tMemória usada em Kbytes: %ld\n", uso_final.ru_maxrss);
 	printf("\tTempo de execução %.7lfs\n", difftime(fim.tv_sec, inicio.tv_sec)
 									+ (double) (fim.tv_usec - inicio.tv_usec)/1000000);
