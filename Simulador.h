31943d324106aff7de54c2981756c599a50d30a8
Pedro Saman <pedrosaman96@gmail.com>
2016-11-09 14:53:55 -0200

    Changed the second simulator name

--- Simulador.h
+++ Simulador.h
@@ -1,7 +1,7 @@
 #ifndef Simulador
 #define Simulador
 
-void Chamou_Elevador_Inteligente(TipoFila *Fila, int carga_maxima);
+void Chamou_Elevador_B(TipoFila *Fila, int carga_maxima);
 int Chamou_Elevador_FIFO(TipoFila *Fila,int Tempo_Atual,int Andar_Atual);
 void Carrega_Fila_com_os_Dados(TipoFila *Fila, char Qual_Arquivo[],int *andar_maximo,int *numero_de_eventos,int *carga_maxima);
 
